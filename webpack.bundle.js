9bcb23d6805ad84467ab92b4dcebddb324020444
Gabriel <101530455+IPlayer0neI@users.noreply.github.com>
2025-06-29 21:59:37 -0300

    change webpack

--- webpack.bundle.js
+++ webpack.bundle.js
@@ -6,8 +6,10 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        library: 'UWUFufuApiWrapper',
+        library: 'uwufufu-wrapper',
         libraryTarget: 'umd',
+        globalObject: 'this',
+        libraryExport: "default",    
     },
     module: {
         rules: [
